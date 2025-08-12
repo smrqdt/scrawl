2c5fcecc68b753befd140c197493a5e61f46479e
Simon Marquardt (smrqdt) <simon.marquardt@posteo.de>
2025-08-12 22:56:11 +0200

    refresh after forking repo
    - update goquery and replace deprecated `goquery.NewDocument`
    - add custom User-Agent because the default didn’t work on the tested
      site
    - add `-skip-existing` flag to avoid downloading the same file multiple
      times
    - limited concurrent downloads to 5
    - fixed bug breaking relative links

--- go.mod
+++ go.mod
@@ -1,10 +1,12 @@
-module foosoft.net/projects/scrawl
+module github.com/smrqdt/scrawl
 
-go 1.18
+go 1.23.0
 
-require github.com/PuerkitoBio/goquery v1.8.0
+toolchain go1.24.6
+
+require github.com/PuerkitoBio/goquery v1.10.3
 
 require (
-	github.com/andybalholm/cascadia v1.3.1 // indirect
-	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	golang.org/x/net v0.39.0 // indirect
 )
--- scrawl.go
+++ scrawl.go
@@ -17,8 +17,12 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-func scrape(url, css, attr string) ([]string, error) {
-	doc, err := goquery.NewDocument(url)
+func scrape(url *url.URL, css, attr string) ([]string, error) {
+
+	buff := &bytes.Buffer{}
+	download(url, buff)
+
+	doc, err := goquery.NewDocumentFromReader(buff)
 	if err != nil {
 		return nil, err
 	}
@@ -39,14 +43,24 @@ func scrape(url, css, attr string) ([]string, error) {
 	return assets, nil
 }
 
-func download(url string, w io.Writer) error {
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
+func download(url *url.URL, w io.Writer) error {
+	client := &http.Client{}
 
-	io.Copy(w, resp.Body)
+	req, err := http.NewRequest("GET", url.String(), nil)
+	if err != nil {
+		return fmt.Errorf("could not create request: %w", err)
+	}
+
+	req.Header.Add("User-Agent", "scrawl/v1")
+
+	res, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to fetch resource: %w", err)
+	}
+
+	defer res.Body.Close()
+
+	io.Copy(w, res.Body)
 	return nil
 }
 
@@ -62,17 +76,17 @@ func export(path string, r io.Reader) error {
 }
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "Usage: %s [options] url selector [path]\n", path.Base(os.Args[0]))
-	fmt.Fprintf(os.Stderr, "https://foosoft.net/projects/scrawl/\n\n")
-	fmt.Fprintf(os.Stderr, "Parameters:\n")
+	fmt.Fprintf(os.Stderr, "Usage: %s [options] url selector\n", path.Base(os.Args[0]))
+	fmt.Fprintln(os.Stderr, "Parameters:")
 	flag.PrintDefaults()
 }
 
 func main() {
 	var (
-		attr    = flag.String("attr", "", "attribute to query")
-		dir     = flag.String("dir", ".", "output directory")
-		verbose = flag.Bool("verbose", false, "verbose output")
+		attr         = flag.String("attr", "", "attribute to query")
+		dir          = flag.String("dir", ".", "output directory")
+		verbose      = flag.Bool("verbose", false, "verbose output")
+		skipExisting = flag.Bool("skip-existing", true, "skip files that already exist")
 	)
 
 	flag.Usage = usage
@@ -97,40 +111,48 @@ func main() {
 		log.Printf("scraping page '%s'", baseRaw)
 	}
 
-	assetsRaw, err := scrape(baseRaw, css, *attr)
+	assetsRaw, err := scrape(base, css, *attr)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	var wg sync.WaitGroup
+	sem := make(chan int, 5)
 	for _, assetRaw := range assetsRaw {
 		wg.Add(1)
 		go func(assetRaw string) {
 			defer wg.Done()
+			sem <- 1
+			defer func() { <-sem }()
 
 			if *verbose {
 				log.Printf("parsing asset string '%s'", assetRaw)
 			}
 
-			asset, err := url.Parse(assetRaw)
+			assetURL, err := url.Parse(assetRaw)
 			if err != nil {
 				log.Fatal(err)
 			}
 
-			if !asset.IsAbs() {
-				asset = asset.ResolveReference(base)
-			}
+			assetURL = base.ResolveReference(assetURL)
 
 			if *verbose {
-				log.Printf("downloading file '%s'", asset.String())
+				log.Printf("downloading file '%s'", assetURL.String())
+			}
+
+			path := filepath.Join(*dir, filepath.Base(assetURL.Path))
+
+			if *skipExisting {
+				if _, err := os.Stat(path); err == nil {
+					log.Printf("skipping existing file %s", assetURL.String())
+					return
+				}
 			}
 
 			var buff bytes.Buffer
-			if err := download(asset.String(), &buff); err != nil {
+			if err := download(assetURL, &buff); err != nil {
 				log.Fatal(err)
 			}
-
-			path := filepath.Join(*dir, filepath.Base(asset.Path))
 
 			if *verbose {
 				log.Printf("writing file '%s'", path)
