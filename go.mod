b109dc8311acc2c3f85c52cb47957969929312ed
Simon Marquardt (smrqdt) <simon.marquardt@posteo.de>
2025-08-13 02:02:05 +0200

    use zerolog with its ConsoleWriter for more readable log output

--- go.mod
+++ go.mod
@@ -4,9 +4,15 @@ go 1.23.0
 
 toolchain go1.24.6
 
-require github.com/PuerkitoBio/goquery v1.10.3
+require (
+	github.com/PuerkitoBio/goquery v1.10.3
+	github.com/rs/zerolog v1.34.0
+)
 
 require (
 	github.com/andybalholm/cascadia v1.3.3 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	golang.org/x/net v0.39.0 // indirect
+	golang.org/x/sys v0.35.0 // indirect
 )
--- scrawl.go
+++ scrawl.go
@@ -5,7 +5,6 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"net/url"
 	"os"
@@ -15,16 +14,24 @@ import (
 	"sync"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
+
+var _, debug = os.LookupEnv("DEBUG")
+var client = &http.Client{}
 
 func scrape(url *url.URL, css, attr string) ([]string, error) {
 
 	buff := &bytes.Buffer{}
-	download(url, buff)
+	err := download(url, buff)
+	if err != nil {
+		return nil, fmt.Errorf("could not download base url: %w", err)
+	}
 
 	doc, err := goquery.NewDocumentFromReader(buff)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("could not parse website: %w", err)
 	}
 
 	var assets []string
@@ -44,8 +51,6 @@ func scrape(url *url.URL, css, attr string) ([]string, error) {
 }
 
 func download(url *url.URL, w io.Writer) error {
-	client := &http.Client{}
-
 	req, err := http.NewRequest("GET", url.String(), nil)
 	if err != nil {
 		return fmt.Errorf("could not create request: %w", err)
@@ -58,6 +63,10 @@ func download(url *url.URL, w io.Writer) error {
 		return fmt.Errorf("failed to fetch resource: %w", err)
 	}
 
+	if res.StatusCode < 200 || res.StatusCode > 299 {
+		return fmt.Errorf("received HTTP status %v", res.Status)
+	}
+
 	defer res.Body.Close()
 
 	io.Copy(w, res.Body)
@@ -67,7 +76,7 @@ func download(url *url.URL, w io.Writer) error {
 func export(path string, r io.Reader) error {
 	out, err := os.Create(path)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("could not create file: %w", err)
 	}
 	defer out.Close()
 
@@ -83,85 +92,128 @@ func usage() {
 
 func main() {
 	var (
-		attr         = flag.String("attr", "", "attribute to query")
-		dir          = flag.String("dir", ".", "output directory")
-		verbose      = flag.Bool("verbose", false, "verbose output")
-		skipExisting = flag.Bool("skip-existing", true, "skip files that already exist")
+		attr      = flag.String("attr", "", "attribute to query")
+		dir       = flag.String("dir", ".", "output directory")
+		verbose   = flag.Bool("verbose", false, "verbose output")
+		overwrite = flag.Bool("overwrite", true, "overwrite files that already exist (will be skipped otherwise)")
 	)
 
 	flag.Usage = usage
 	flag.Parse()
 
+	zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	if *verbose {
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	}
+	if debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
 	if flag.NArg() != 2 {
 		flag.Usage()
-		os.Exit(2)
+		log.Fatal().Strs("args", os.Args[1:]).Msg("missing arguments")
 	}
 
-	var (
-		baseRaw = flag.Arg(0)
-		css     = flag.Arg(1)
-	)
+	baseRaw := flag.Arg(0)
+	css := flag.Arg(1)
 
 	base, err := url.Parse(baseRaw)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal().Err(err).Str("url", baseRaw).Msg("could not parse base URL")
 	}
 
-	if *verbose {
-		log.Printf("scraping page '%s'", baseRaw)
-	}
+	log.Info().
+		Str("url", baseRaw).
+		Msg("scraping page")
 
 	assetsRaw, err := scrape(base, css, *attr)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal().Err(err).
+			Str("css", css).
+			Str("url", base.String()).
+			Str("attr", *attr).
+			Msg("could not scrape assets")
 	}
 
+	if len(assetsRaw) == 0 {
+		log.WithLevel(zerolog.FatalLevel).Msg("no assets found")
+		os.Exit(2)
+	}
+
+	log.Info().Int("results", len(assetsRaw)).Msg("assets found")
 	var wg sync.WaitGroup
 	sem := make(chan int, 5)
-	for _, assetRaw := range assetsRaw {
+	for jobID, assetRaw := range assetsRaw {
 		wg.Add(1)
-		go func(assetRaw string) {
+		log.Debug().
+			Int("job", jobID).
+			Str("target", assetRaw).
+			Msg("queuing job")
+		go func(jobID int, assetRaw string) {
 			defer wg.Done()
 			sem <- 1
 			defer func() { <-sem }()
 
-			if *verbose {
-				log.Printf("parsing asset string '%s'", assetRaw)
-			}
+			log.Debug().
+				Int("job", jobID).
+				Str("target", assetRaw).
+				Msg("starting download job")
 
 			assetURL, err := url.Parse(assetRaw)
 			if err != nil {
-				log.Fatal(err)
+				log.Fatal().Err(err).
+					Int("job", jobID).
+					Str("target", assetRaw).
+					Msg("could not parse asset target into URL")
 			}
 
 			assetURL = base.ResolveReference(assetURL)
-
-			if *verbose {
-				log.Printf("downloading file '%s'", assetURL.String())
-			}
-
 			path := filepath.Join(*dir, filepath.Base(assetURL.Path))
 
-			if *skipExisting {
+			if !*overwrite {
 				if _, err := os.Stat(path); err == nil {
-					log.Printf("skipping existing file %s", assetURL.String())
+					log.Warn().
+						Int("job", jobID).
+						Str("url", assetURL.String()).
+						Msg("skipping existing file")
 					return
 				}
 			}
 
+			log.Info().
+				Int("job", jobID).
+				Str("url", assetURL.String()).
+				Msg("downloading file")
+
 			var buff bytes.Buffer
 			if err := download(assetURL, &buff); err != nil {
-				log.Fatal(err)
+				log.Error().Err(err).
+					Int("job", jobID).
+					Str("url", assetURL.String()).
+					Msg("could not download asset")
+				return
 			}
 
-			if *verbose {
-				log.Printf("writing file '%s'", path)
-			}
+			log.Debug().
+				Int("job", jobID).
+				Str("filename", path).
+				Msg("writing file")
 
 			if err := export(path, &buff); err != nil {
-				log.Fatal(err)
+				log.Fatal().Err(err).
+					Int("job", jobID).
+					Str("filename", path).
+					Msg("could not write file")
+				return
 			}
-		}(assetRaw)
+
+			log.Info().Int("job", jobID).
+				Str("path", path).
+				Str("url", assetURL.String()).
+				Msg("successful download")
+
+		}(jobID, assetRaw)
 	}
 
 	wg.Wait()
